1df14019de7e4eb9ca3a0bdf38a0c819e9bdde7b
Taya <tayakolodeeva@yandex.ru>
2022-11-28 22:00:54 +0300

    program5

--- z.5/z5.py
+++ z.5/z5.py
@@ -1,12 +1,6 @@
 import datetime 
 
-min_n = (input('Введите диапазон от :'))
-max_n = (input('до :'))
-
 def get_rand():
     return datetime.datetime.now().microsecond%10
-
 n = get_rand()
-
-print(int(len(str(min_n))))
 print(n)
